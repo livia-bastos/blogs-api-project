7fcd2fcb0585899f70cc073a4cd632111bd135a4
Livia Bastos <livialbastos@gmail.com>
2023-08-29 19:07:13 -0300

    req 11

--- src/models/PostCategory.js
+++ src/models/PostCategory.js
@@ -1,23 +1,22 @@
 const PostCategoriesModel = (sequelize, DataTypes) => {
-  const PostCategories = sequelize.define('PostCategories', {
-    post_id: {type: DataTypes.INTEGER, primaryKey: true, foreignKey: true },
-    category_id: { type: DataTypes.INTEGER, primaryKey: true, foreignKey: true },
+  const PostCategories = sequelize.define('PostCategory', {
+    postId: {type: DataTypes.INTEGER, primaryKey: true, foreignKey: true },
+    categoryId: { type: DataTypes.INTEGER, primaryKey: true, foreignKey: true },
   }, { underscored: true, timestamps: false, tableName: 'PostCategories' });
 
-  // PostCategories.associate = (models) => {
-  //   models.Book.belongsToMany(models.User, {
-  //     as: 'users',
-  //     through: UserBook,
-  //     foreignKey: 'bookId', // se refere ao id de Book na tabela de `users_books`
-  //     otherKey: 'userId', // se refere a outra chave de `users_books`
-  //   });
-  //   models.User.belongsToMany(models.Book, {
-  //     as: 'books',
-  //     through: UserBook,
-  //     foreignKey: 'userId', // se refere ao id de User na tabela de `users_books`
-  //     otherKey: 'bookId',
-  //   });
-  // };
+  PostCategories.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      through: PostCategories,
+      foreignKey: 'postId', 
+      otherKey: 'categoryId', 
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'category',
+      through: PostCategories,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
     
 
   return PostCategories;
